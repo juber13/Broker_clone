bc18d1d25cd9b771cc918706a55df62e28597f8f
juber <juberk1010@gmail.com>
2024-04-05 02:18:06 +0530

    add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,22 +1,13 @@
 const userModel = require('../models/user.model');
 const errorHandler = require('../utills/error');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
 
 const signUp = async (req, res, next) => {
-    const { firstName, lastName, email, password, phone, f } = req.body;
-
-    if (!firstName || !lastName || !email || !password || !phone) {
-        return res.json({
-            success: false,
-            message: "All fields are required"
-        })
-    }
-
-    if(password.length < 8){
-        return errorHandler(500 , "password length should be eight character")
-    }
+    const { email, password, name } = req.body;
 
     try {
-        const newUser = await userModel.create({ firstName, lastName, email, password, phone })
+        const newUser = await userModel.create({ name, email, password })
         res.status(201).json({
             success: true,
             message: "User sign up successfully",
@@ -26,9 +17,22 @@ const signUp = async (req, res, next) => {
     } catch (err) {
         next(err)
     }
-
-
-
 }
 
-module.exports = signUp;
+
+const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const validUser = await userModel.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(401, "Wron credentials");
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+}
+
+module.exports = { signUp, signIn };
