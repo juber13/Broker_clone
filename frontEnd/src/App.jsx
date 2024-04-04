0d0cad771a1e97025f4802d8267655f47543e9f8
juber <juberk1010@gmail.com>
2024-04-04 14:25:59 +0530

    create pages and routes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
 
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-300">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sing-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
