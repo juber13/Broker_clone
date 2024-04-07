c496e366171c3a057ab436f3e4778a62bbbf0403
juber <juberk1010@gmail.com>
2024-04-07 16:21:02 +0530

    add api key

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -6,7 +6,7 @@ export const verifyToken = (req, res, next) => {
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-  jwt.verify(token, process.env.hellojuebr4ur833jds23, (err, user) => {
+  jwt.verify(token,"hellojuebr4ur833jds23", (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
 
     req.user = user;
